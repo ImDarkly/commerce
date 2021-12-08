3b91648f34819f5d16a1a891f7195a08fdb19b1a
ImDarkly <66973381+ImDarkly@users.noreply.github.com>
2021-12-08 02:03:49 +0200

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
 function OpenList() {
     var element = document.getElementById("nav");
     element.classList.toggle("nav-active");
+    var element = document.getElementById("nav-blur");
+    element.classList.toggle("hide");
 }
